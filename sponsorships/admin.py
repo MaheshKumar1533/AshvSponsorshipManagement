1584bb113c5de7b07aa2149d3259b25579d55997
Mahesh <maheshkumarvmk0@gmail.com>
2026-02-12 21:50:30 +0530

    added

--- sponsorships/admin.py
+++ sponsorships/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from sponsorships.models import *
 # Register your models here.
+admin.site.register(Sponsor)
+admin.site.register(Settlement)
+admin.site.register(SponsorshipCategory)
